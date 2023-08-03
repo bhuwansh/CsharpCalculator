d8677f0ac293710070ae6f4b1f784df811c02618
BHUWAN SHARMA <bsbsharma47@gmail.com>
2023-08-03 18:06:14 +0530

    Update calC.cs

--- calC.cs
+++ calC.cs
@@ -5,8 +5,36 @@ using System;
 
 public class calculator
 {
+  
+    public int Add (int A , int B)
+    {
+        return A + B;
+    }
+    
+     public int Sub (int A , int B)
+    {
+        return A - B;
+    }
+}
+
+class program
+{
     public static void Main(string[] args)
     {
-        Console.WriteLine ("i am a c# calculator.");
+    
+      calculator cl = new  calculator();
+      Console.WriteLine ("calC start");
+      
+      Console.WriteLine ("Enter two Number : ");
+      Console.Write("Number 1: ");
+      int num1 = Convert.ToInt32(Console.ReadLine());
+      
+       Console.Write("Number 2: ");
+      int num2 = Convert.ToInt32(Console.ReadLine());
+      
+      
+      
+      
+      
     }
 }
