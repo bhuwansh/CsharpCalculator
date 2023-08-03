a128c563790536065744d95acc62f57f27e1db3c
BHUWAN SHARMA <bsbsharma47@gmail.com>
2023-08-03 17:46:16 +0530

    Create calC.cs

---
+++ calC.cs
@@ -0,0 +1,12 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class calculator
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("i am a c# calculator.");
+    }
+}
